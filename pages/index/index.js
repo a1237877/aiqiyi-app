a19b77d2c72a14a628257cd05d0fde236dfd4991
a1237877 <487333601@qq.com>
2019-06-04 23:38:32 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,11 +9,17 @@ Page({
     autoplay:true,
     interval:2000,
     duration:1000,
+    hot_data:[],
+    news_lists:[]
   },
   onLoad(){
     const index_slides = app.globalData.index_slides;
+    const hot_data=app.globalData.hot_data;
+    const news_lists = app.globalData.news_lists;
     this.setData({
       index_slides,
+      hot_data,
+      news_lists
     })
   },
   //事件处理函数
--- utils/mock.js
+++ utils/mock.js
@@ -15,5 +15,39 @@ export default {
             id: 31,
             slide_url: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1557813748335&di=2603118d7f5adf4ac275c722cceae6e6&imgtype=0&src=http%3A%2F%2Fpic7.qiyipic.com%2Fcommon%2F20141008%2F872410.jpg%3Fsrc%3Dfocustat_6_20130410_16"
         }
+    ],
+    hot_data:[
+        {
+            hot_urls: "../channel/phone/phone",
+            hot_img: "../../images/1.jpg",
+            hot_title: "三生三世"
+        },
+        {
+            hot_urls: "../channel/phone/phone",
+            hot_img: "../../images/2.jpg",
+            hot_title: "河神"
+        },
+        {
+            hot_urls: "../channel/phone/phone",
+            hot_img: "../../images/3.jpg",
+            hot_title: "双世宠妃"
+        }
+
+    ],
+    news_lists:[
+        {
+            id: 4,
+            news_img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559356496&di=107c8d30aabf3d5ab036c82b1d5c4f72&imgtype=jpg&er=1&src=http%3A%2F%2Fcrawl.nosdn.127.net%2Fnbotreplaceimg%2F4ce9c743e6c02f6777d22278e2ef8bc3%2F5f0f46733ff2520a50fa813b2e274707.jpg",
+            header: "史上最牛逼演唱会",
+            description: "快来感受一下吧", 
+           
+            
+        },
+        {
+            id: 5,
+            news_img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559621133383&di=1d3bfb1b241d765a7a91442fd1c4fe5f&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_mini%2Cc_zoom%2Cw_640%2Fimages%2F20170713%2Fc9a81abf40564ac1b7b822e3a2fb5429.jpg",
+            header: "甜甜的动漫",
+            description: "傻逼"
+        }
     ]
 }
